0080f6a51650b6f8aea92e07d2e5730384b55162
Anna Pobedonostseva <zebra9999@rambler.ru>
2023-12-06 15:32:16 +0300

    First review

---
+++ src/configs.py
@@ -0,0 +1,63 @@
+import argparse
+import logging
+from logging.handlers import RotatingFileHandler
+
+from constants import BASE_DIR
+
+# Описание формата логов:
+# Время записи – Уровень сообщения – Cообщение.
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+# Указываем формат времени.
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+
+
+def configure_argument_parser(available_modes):
+    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser.add_argument(
+        'mode',
+        choices=available_modes,
+        help='Режимы работы парсера'
+    )
+    # Новый аргумент.
+    parser.add_argument(
+        '-c',
+        '--clear-cache',
+        action='store_true',
+        help='Очистка кеша'
+    )
+    # Новый аргумент --output вместо аргумента --pretty
+    parser.add_argument(
+        '-o',
+        '--output',
+        choices=('pretty', 'file'),
+        help='Дополнительные способы вывода данных')
+
+    return parser
+
+
+# Добавьте новую функцию и напишите нужные команды.
+def configure_logging():
+    # Сформируйте путь до директории logs.
+    log_dir = BASE_DIR / 'logs'
+    # Создайте директорию.
+    log_dir.mkdir(exist_ok=True)
+    # Отсюда начинается новый код!
+    # Получение абсолютного пути до файла с логами.
+    log_file = log_dir / 'parser.log'
+
+    # Инициализация хендлера с ротацией логов.
+    # Максимальный объём одного файла — десять в шестой степени байт (10**6),
+    # максимальное количество файлов с логами — 5.
+    rotating_handler = RotatingFileHandler(
+        log_file, maxBytes=10 ** 6, backupCount=5,
+        encoding='utf-8'
+    )
+    # Базовая настройка логирования basicConfig.
+    logging.basicConfig(
+        datefmt=DT_FORMAT,
+        format=LOG_FORMAT,
+        # Уровень записи логов.
+        level=logging.INFO,
+        # Вывод логов в терминал.
+        handlers=(rotating_handler, logging.StreamHandler())
+    )
---
+++ src/outputs.py
@@ -0,0 +1,65 @@
+from prettytable import PrettyTable
+import datetime as dt
+import csv
+import logging
+
+from constants import BASE_DIR, DATETIME_FORMAT
+
+
+def control_output(results, cli_args):
+    # Чтобы не обращаться дважды к атрибуту объекта в условиях if, elif,
+    # сохраним значение в переменную.
+    output = cli_args.output
+    if output == 'pretty':
+        # Вывод данных в PrettyTable.
+        pretty_output(results)
+    elif output == 'file':
+        # Вывод данных в файл csv. Саму функцию напишем позже.
+        file_output(results, cli_args)
+    else:
+        # Вывод данных по умолчанию — в терминал построчно.
+        default_output(results)
+
+
+def default_output(results):
+    # Печатаем список results построчно.
+    for row in results:
+        print(*row)
+
+
+def pretty_output(results):
+    # Инициализируем объект PrettyTable.
+    table = PrettyTable()
+    # В качестве заголовков устанавливаем первый элемент списка.
+    table.field_names = results[0]
+    # Выравниваем всю таблицу по левому краю.
+    table.align = 'l'
+    # Добавляем все строки, начиная со второй (с индексом 1).
+    table.add_rows(results[1:])
+    # Печатаем таблицу.
+    print(table)
+
+
+# Добавьте новую функцию и напишите нужные команды.
+def file_output(results, cli_args):
+    results_dir = BASE_DIR / 'results'
+    results_dir.mkdir(exist_ok=True)
+    # Отсюда начинается новый код.
+    # Получаем режим работы парсера из аргументов командной строки.
+    parser_mode = cli_args.mode
+    # Получаем текущие дату и время.
+    now = dt.datetime.now()
+    # Сохраняем текущие дату-время в указанном формате.
+    # Результат будет выглядеть вот так: 2021-06-18_07-40-41.
+    now_formatted = now.strftime(DATETIME_FORMAT)
+    # Собираем имя файла из полученных переменных:
+    # «режим работы программы» + «дата и время записи» + формат (.csv).
+    file_name = f'{parser_mode}_{now_formatted}.csv'
+    # Получаем абсолютный путь к файлу с результатами.
+    file_path = results_dir / file_name
+    with open(file_path, 'w', encoding='utf-8') as f:
+        # Создаём «объект записи» writer.
+        writer = csv.writer(f, dialect='unix')
+        # Передаём в метод writerows список с результатами парсинга.
+        writer.writerows(results)
+    logging.info(f'Файл с результатами был сохранён: {file_path}')
