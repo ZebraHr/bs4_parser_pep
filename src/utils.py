186ee3d06194b0dbabfa2a2848b06011634e024c
Anna Pobedonostseva <zebra9999@rambler.ru>
2023-12-06 15:36:20 +0300

    Comments removed

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,6 @@ import logging
 from requests import RequestException
 from exceptions import ParserFindTagException
 
-
-# Перехват ошибки RequestException.
 def get_response(session, url):
     try:
         response = session.get(url)
@@ -17,7 +15,6 @@ def get_response(session, url):
         )
 
 
-# Перехват ошибки поиска тегов.
 def find_tag(soup, tag, attrs=None):
     searched_tag = soup.find(tag, attrs=(attrs or {}))
     if searched_tag is None:
